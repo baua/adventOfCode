611939612117402a0db07c7ae6aa850f2b693c6a
Martin Lehmann <martin.lehmann@sirca.org.au>
2017-12-01 17:44:59 +1100

    day 01 part 2

--- 2017/xmas.sh
+++ 2017/xmas.sh
@@ -9,17 +9,27 @@ fi
 day=$1
 
 function day1() {
+    list=12131415
+    list=123123
+    list=123425
     local list; list=$(cat "${inputDir}"/day1.txt)
 
     local -i sum=0
+    local -i sum2=0
     local -i last current
+    local -i len; let len=${#list}
+    local -i half; let half=$(( ${#list} / 2 ))
     last=${list:((${#list}-1)):1}
+    last2=${list:((half)):1}
     for i in $(seq 0 $((${#list}-1))); do
         current=${list:$i:1}
         (( last == current )) && ((sum+=current))
+        (( last2 == current )) && ((sum2+=current))
         let last=current
+        let last2=${list:$(((i+half+1) % len)):1}
     done
-    echo "sum=${sum}"
+    echo "sum_part1=${sum}"
+    echo "sum_part2=${sum2}"
 }
 
 eval -- day"${day}"
