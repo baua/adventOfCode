a9a2336b7f4977e12db060d58f9021c0a2e0ed1c
Martin Lehmann <martin.lehmann@sirca.org.au>
2017-12-04 15:18:50 +1100

    Day1 - Finalised

--- 2017/xmas.hs
+++ 2017/xmas.hs
@@ -4,26 +4,30 @@ import Data.List
 import System.FilePath
 
 inputDir = "/home/mlehmann/study/adventOfCode/2017/inputs/"
-days = [ day1 ]
+days = [ day1Main ]
 
 day1Main = do
     c <- readFile $ inputDir </> "day1.txt"
-    print $ day1part1 c
+    -- print $ day1part1 c
     print $ day1part2 c
 
--- listNumbers = map (read :: String -> Int)
---               . map (\ x -> [x])
-  --             . filter isDigit $ str
+-- DAY1 Start
+listNumbers = map (read :: String -> Int)
+               . map (\ x -> [x])
+               . filter isDigit
 
-day1part1 str = let listNumbers = map (read :: String -> Int)
-                             . map (\ x -> [x])
-                             . filter isDigit $ str
-           in sum
+sumUpEquals = sum
               . map (\ (a,b) -> a)
               . filter (uncurry (==))
-              . zip listNumbers $ ( tail $ cycle listNumbers )
 
-day1par2 str = 
+day1part1 str = let numbers = listNumbers str
+           in sumUpEquals . zip numbers $ ( tail $ cycle numbers )
+
+day1part2 str = let numbers = listNumbers str
+                    halfNumbers = (div (length numbers) 2 )
+           in sumUpEquals . zip numbers $ drop halfNumbers $ cycle numbers
+
+-- DAY1 End
 
 rInt :: String -> Int
 rInt = read
@@ -36,3 +40,4 @@ main = do
 
 -- results
 -- day01_01 1031
+-- day01_02 1080
